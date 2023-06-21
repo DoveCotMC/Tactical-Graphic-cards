0b54b1613faf164bc35a1a5393c1df02de2b668f
MikhailTapio <mikhailtapio13468@163.com>
2023-06-21 17:43:28 +0800

    Add config

--- src/main/java/mod/arrokoth/tacticalcards/TacticalCards.java
+++ src/main/java/mod/arrokoth/tacticalcards/TacticalCards.java
@@ -1,9 +1,12 @@
 package mod.arrokoth.tacticalcards;
 
+import mod.arrokoth.tacticalcards.config.CommonConfig;
 import mod.arrokoth.tacticalcards.utils.RegistryHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TacticalCards.MOD_ID)
@@ -12,6 +15,7 @@ public class TacticalCards {
     public static final String MOD_NAME = "Tactical Cards";
     public TacticalCards() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CommonConfig.CFG);
         RegistryHandler.register(bus);
         MinecraftForge.EVENT_BUS.register(this);
     }
